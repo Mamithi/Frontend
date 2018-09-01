75eb0233bd799ba9e714865b77817c25dba47afa
Mamithi <19lawrence93@gmail.com>
2018-09-01 13:47:29 +0300

    Scroll through testimonials

--- Awesome/js/main.js
+++ Awesome/js/main.js
@@ -46,10 +46,28 @@
         })
     }
 
+    const onTestimonialChange = () => {
+        let firstChild, lastChild;
+        const prevArrow = document.querySelector("#aw-testimonials-prev");
+        const nextArrow = document.querySelector("#aw-testimonials-next");
+        const testimonials = document.querySelector(".aw-testimonials ul");
+
+        document.addEventListener("click", () => {
+            if (event.target === prevArrow) {
+                lastChild = testimonials.lastElementChild;
+                testimonials.insertAdjacentElement("afterbegin", lastChild);
+            } else if (event.target === nextArrow) {
+                firstChild = testimonials.firstElementChild;
+                testimonials.insertAdjacentElement("beforeend", firstChild);
+            }
+        })
+    }
+
     window.addEventListener("scroll", () => {
         addMenuBackground();
     })
 
     onNavItemClick();
+    onTestimonialChange();
 
 })();
