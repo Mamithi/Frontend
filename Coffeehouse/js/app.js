8ce02cc978c4a1e3e9c000b9e017d54e15b07887
Mamithi <19lawrence93@gmail.com>
2018-11-20 19:56:29 +0300

    Add the preloader

---
+++ Coffeehouse/js/app.js
@@ -0,0 +1,7 @@
+// hide preloader
+// all the images scripts link shave finished loading
+
+//window event listener
+window.addEventListener('load', function() {
+    document.querySelector('.cs-preloader').style.display="none";
+});
