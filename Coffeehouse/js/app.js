0c87a72b94eb2e390551c0f4ea98e52a4dbc734a
Mamithi <19lawrence93@gmail.com>
2018-11-20 20:13:20 +0300

    Toggle button

--- Coffeehouse/js/app.js
+++ Coffeehouse/js/app.js
@@ -5,3 +5,8 @@
 window.addEventListener('load', function() {
     document.querySelector('.cs-preloader').style.display="none";
 });
+
+// nav button
+document.querySelector(".cs-nav-btn").addEventListener('click', function() {
+    document.querySelector(".cs-nav").classList.toggle("cs-nav-show");
+});
